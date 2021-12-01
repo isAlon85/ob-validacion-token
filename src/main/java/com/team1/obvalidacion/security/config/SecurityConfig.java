d05bb346b6f31c446c0e64a31305a566901afc80
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2021-12-01 14:27:54 +0100

    update url cors policy

--- src/main/java/com/team1/obvalidacion/security/config/SecurityConfig.java
+++ src/main/java/com/team1/obvalidacion/security/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(List.of("http://localhost"/*, "https://angular-springboot-*.vercel.app"*/));
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"/*, "https://ob-angular-spring.vercel.app*/));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://hungry-hermann-91b401.netlify.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
